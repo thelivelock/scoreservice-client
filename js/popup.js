5d3bd9c052a17224a8e06b72dbbc524884a0ed92
Oskar Vatic <oskarsmit@hotmail.com>
2019-12-06 19:46:35 +0100

    Implement email validation and error handling

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const subButton = document.getElementById("subButton");
+
+const validateEmail = (email) => {
+  // RFC 2822 
+  const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  return regex.test(String(email).toLowerCase());
+};
+
+subButton.onclick = () => { 
+  const email = $('.form-control').val();
+  const isEmailValid = validateEmail(email);
+
+  if (isEmailValid) {
+    $('#emailInputComponent').hide();
+    $('#invalidEmailMessage').css('display', 'none');
+    $('#postSubscribeMessage').css('display', 'block');
+  } else {
+    $('#invalidEmailMessage').css('display', 'block');
+    $('#invalidEmailMessage').css('color', 'red');
+  }
+};
